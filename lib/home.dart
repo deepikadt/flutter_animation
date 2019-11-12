d0a8e40f2944b88a87d3025f85990ea0e567115a
deepikadt <>
2019-11-12 16:17:23 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 class FlareAnimationPage extends StatefulWidget {
   @override
@@ -8,8 +9,15 @@ class FlareAnimationPage extends StatefulWidget {
 class _FlareAnimationPageState extends State<FlareAnimationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:Container(),
-      
+    return Scaffold(
+      body: Center(
+        child: FlareActor(
+          'assets/circular1.flr',
+          animation: "Untitled",
+          fit: BoxFit.contain,
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
